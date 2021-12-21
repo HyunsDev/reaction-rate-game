f55b60fa52c4bd73cab334ccbc789504736d773c
혀느현스 <hyuns@hyuns.dev>
2021-12-21 11:02:47 +0900

    기초 코드 세팅

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+const Game = (props) => {
+
+
+
+  const startGame = () => {
+
+  }
+
+  return (
+    <div className='game'>
+      <div></div>
+      <div className='title'></div>
+      <div className='name'>POS 반응 속도 게임</div>
+    </div>
+  ) 
+}
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
